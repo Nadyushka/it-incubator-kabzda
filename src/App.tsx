69db8684d1d54970c78e235dc7547a99d4c81c7d
Nadyushka <12nadya11@gmail.com>
2023-01-05 21:28:10 +0300

    select add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Rating from "./Components/Rating/Rating";
 import Select from './Components/Select/Select';
 
 function App() {
+
+    const [activeTitle,setActiveTitle] = useState<string>('Minsk')
+
     return (
         <div className="App">
             {/*<Rating/>*/}
 
 
-            <Select items={[
+            <Select
+                activeTitle={activeTitle}
+                setActiveTitle={setActiveTitle}
+                items={[
                 {value: '1', title: 'Minsk'},
                 {value: '2', title: 'Moscow'},
                 {value: '3', title: 'Kiev'}
--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import s from './select.module.css'
 
 type ItemType = {
-    value:string
-    title:string
+    value: string
+    title: string
 }
 
 type PropsType = {
     items: ItemType[]
+    activeTitle: string | null
+    setActiveTitle: (newTitle: string) => void
 }
 
-const Select = (props:PropsType) => {
-    
+const Select = (props: PropsType) => {
+
+    const [collapsed, setCollapsed] = useState<boolean>(false)
+    const setVisibility = () => setCollapsed(!collapsed)
+
+
     return (
-        <span>
-            <span>Active value</span>
-            <div>
-                {props.items.map(i=>{
-                    return (
-                        <div key={i.value}>{i.title}</div>
-                    )
-                }
-                )}
-            </div>
+        <span className={s.selectBody}>
+            <span className={s.selectActiveTitle} onClick={setVisibility}>{props.activeTitle}</span>
+            {collapsed &&
+                <div>
+                    {props.items.map(i => {
+
+                        const setActiveTitleOnClick =() => {
+                            props.setActiveTitle(i.title)
+                            setVisibility()
+                        }
+
+                            return (
+                                <div
+                                    onClick={setActiveTitleOnClick}
+                                    key={i.value}>{i.title}</div>
+                            )
+                        }
+                    )}
+
+                </div>
+            }
         </span>
-    );
+    )
+        ;
 };
 
 export default Select;
