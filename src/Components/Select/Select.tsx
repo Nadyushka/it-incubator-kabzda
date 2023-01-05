55150fbfe052b3e872088402a77a465ab7053d9c
Nadyushka <12nadya11@gmail.com>
2023-01-05 22:01:48 +0300

    select add triangle

--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -15,24 +15,34 @@ type PropsType = {
 const Select = (props: PropsType) => {
 
     const [collapsed, setCollapsed] = useState<boolean>(false)
+    const [hoveredTitle, setHoveredTitle] = useState<string>('Minsk')
+
     const setVisibility = () => setCollapsed(!collapsed)
 
 
     return (
         <span className={s.selectBody}>
-            <span className={s.selectActiveTitle} onClick={setVisibility}>{props.activeTitle}</span>
+            <span className={s.selectActiveTitle} onClick={setVisibility}>{props.activeTitle}
+                <span className={s.triangle + ' '+ (collapsed ? s.triangleBottom : s.triangleTop )}></span>
+            </span>
             {collapsed &&
-                <div>
+                <div className={s.selectTitles}>
                     {props.items.map(i => {
 
-                        const setActiveTitleOnClick =() => {
-                            props.setActiveTitle(i.title)
-                            setVisibility()
-                        }
+                            const setActiveTitleOnClick = () => {
+                                props.setActiveTitle(i.title)
+                                setVisibility()
+                            }
 
                             return (
                                 <div
+                                    className={hoveredTitle === i.title ? s.hoveredTitle : s.normalTitle}
                                     onClick={setActiveTitleOnClick}
+                                    onMouseOver={() => {
+                                        setHoveredTitle(i.title);
+                                       // props.setActiveTitle(i.title)
+                                    }
+                                    }
                                     key={i.value}>{i.title}</div>
                             )
                         }
